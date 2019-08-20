9df3be8c5a7f88b0fe6c0680dac2b77bb289effc
Jonathan Koss <jonathan_koss@yahoo.com>
2019-08-20 01:03:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,8 @@ def find_the_cheese(array)
   # Your code here
   cheese_array = %w[cheddar gouda camembert]
   
-  array.detect do ||
-    cheese_array.detect {cheese}
+  array.detect do |cheese?|
+    cheese_array.detect {|cheese| cheese == cheese?}
   end 
 end 
 
